d781a18a18a9e7e6897f2dc2880e2a06d47351e2
Kai <kai.pimenta1@gmail.com>
2020-11-17 10:48:03 -0300

    melhorias codigo

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
 
 class App extends Component {
     constructor() {
@@ -26,10 +26,11 @@ class App extends Component {
     }
 
     render() {
-        const filteredRobots = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
-        if (this.state.robots.length === 0) {
+        if (!robots.length) {
             return <h1>Loading</h1>
         } else {
             return (
